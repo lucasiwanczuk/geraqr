4c9e8bd946083bb222839f99a2c73bf670e4610b
Lucas Iwanczuk <lucas.iwanczuk@gmail.com>
2025-04-13 19:38:46 -0300

    Update de estrutura, adicionado imagem como vetor e refatorado objeto principal

--- geraqr/app/page.js
+++ geraqr/app/page.js
@@ -13,25 +13,25 @@ import { Input } from "@/components/ui/input";
 export default function buttonExample() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-            <div className="flex w-full max-w-sm flex-col gap-6">
-            <Card className="w-[350px]">
-      <CardHeader className="grid w-full items-center">
-        <CardTitle>Crie seu agora mesmo seu QR Code!</CardTitle>
-        <CardDescription>Em um clique seu QR code é gerado.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Input id="name" placeholder="Digite um texto ou cole um link" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="grid w-full items-center gap-4">
-        <Button>Gerar QR CODE</Button>
-      </CardFooter>
-    </Card>
+            <div className="w-full max-w-sm flex-col gap-6">
+                <Card className="grid w-full items-center gap-4">
+                  <CardHeader>
+                  <CardTitle className="text-center"><h1>Crie seu agora mesmo seu QR Code!</h1></CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                   <form>
+                      <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                          <img src="#"></img>
+                          <Input id="name" placeholder="Digite um texto ou cole um link" />
+                        </div>
+                      </div>
+                   </form>
+                  </CardContent>
+                  <CardFooter className="grid w-full items-center gap-4">
+                    <Button>Gerar QR CODE</Button>
+                  </CardFooter>
+                </Card>
             </div>
         </div>
     );
